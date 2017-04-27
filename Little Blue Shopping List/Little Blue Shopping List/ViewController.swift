4794fd3c0f043ae6edce03cb53e9f167776fc730
Malcolm Denning <malcolmdenning@outlook.com>
2017-04-28 00:12:16 +1000

    Project start

---
+++ Little Blue Shopping List/Little Blue Shopping List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Little Blue Shopping List
+//
+//  Created by Malcolm Denning on 28/4/17.
+//  Copyright © 2017 Deakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

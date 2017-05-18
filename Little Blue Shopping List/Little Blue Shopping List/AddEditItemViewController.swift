7519a20c3ec4b963071952516b4c3bcf1ce1b890
Malcolm Denning <malcolmdenning@outlook.com>
2017-05-18 22:07:19 +1000

    Added screens for item table and item adding / editing. Implemented stepper code for item add / edit screen

---
+++ Little Blue Shopping List/Little Blue Shopping List/AddEditItemViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddEditItemViewController.swift
+//  Little Blue Shopping List
+//
+//  Created by Malcolm Denning on 18/5/17.
+//  Copyright © 2017 Deakin. All rights reserved.
+//
+
+import UIKit
+
+class AddEditItemViewController: UIViewController {
+
+    @IBOutlet weak var itemQty: UITextField!
+    @IBOutlet weak var itemName: UITextField!
+    @IBOutlet weak var stepper: UIStepper!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        stepper.minimumValue = 0
+        stepper.value = 1
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Code from:
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        itemQty.text = Int(stepper.value).description
+    }
+    
+    @IBAction func itemQtyEditingDidEnd(_ sender: UITextField) {
+        stepper.value = Double(sender.text!)!
+    }
+    
+}
